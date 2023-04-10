2ddf22b61ee902899244c84be8acd86752ca5cc1
Harpreetpreet <preetx025@gmail.com>
2023-04-10 15:50:02 +0530

    add date in eventBlog component

--- src/Components/element/EventBlogCard.js
+++ src/Components/element/EventBlogCard.js
@@ -1,5 +1,4 @@
 import Image from 'next/image';
-import React from 'react';
 
 const EventBlogCard = (props) => {
   return (
@@ -9,7 +8,7 @@ const EventBlogCard = (props) => {
       <h3 className=" py-4 text-lg font-semibold leading-snug hover:text-brand duration-500 ease-in-out">
         {props.title}
       </h3>
-      <div className="absolute flex flex-col  border rounded-md bg-brand m-auto p-3 py-0 items-center text-white">
+      <div className="absolute left-2 top-2 flex flex-col  border-transparent  rounded-md bg-brand m-auto p-3 py-0 items-center text-white">
         <h1 className="text-xl font-bold"> {props.date}</h1>
         <h3 className="text-lg">{props.month}</h3>
       </div>
